8a6484d67523811e272f9a25e67f1e59e3ad495b
torasan <grazinglion@gmail.com>
2024-01-06 17:51:21 +0900

    gitignore: custom_nodes

---
+++ custom_nodes/ComfyUI-Primelabs/__init__.py
@@ -0,0 +1,12 @@
+from . import custom_s3
+
+# A dictionary that contains all nodes you want to export with their names
+# NOTE: names should be globally unique
+NODE_CLASS_MAPPINGS = {
+    **custom_s3.NODE_CLASS_MAPPINGS,
+}
+
+# A dictionary that contains the friendly/humanly readable titles for the nodes
+NODE_DISPLAY_NAME_MAPPINGS = {
+    **custom_s3.NODE_DISPLAY_NAME_MAPPINGS,
+}
